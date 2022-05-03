1f33238b01ffc968fd0337dbc87de0a4e153647c
Bruno Silva <brunno.linkin@gmail.com>
2022-05-03 20:55:37 -0300

    adicao rota/page de login (no auth) e simulação de logout com loadgin plugin animation do quasar

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout view="hHh Lpr fFf">
+    <q-page-container>
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "LoginLayout",
+  setup() {},
+};
+</script>
